c8c9b114bb6d593475a45153cd20a0b3735c7de6
LeeTszFung <77471196+LeeTszFung@users.noreply.github.com>
2022-04-20 01:54:51 +0800

    Problem Controller Update

--- api-server/controller/ProblemController.go
+++ api-server/controller/ProblemController.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"log"
 	"net/http"
+	"strconv"
 	"ustoj-master/common"
 	"ustoj-master/model"
 	"ustoj-master/utils"
@@ -31,7 +32,7 @@ func NewProblemController() IProblemController { // Similar to the interface of 
 
 func (ctl ProblemController) ProblemList(c *gin.Context) {
 	var req vo.ProblemListRequest
-	var p model.Problem
+	var problem model.Problem
 	code := vo.OK
 	var problemID = 0
 	var status = ""
@@ -49,12 +50,12 @@ func (ctl ProblemController) ProblemList(c *gin.Context) {
 			Global_Acceptance: globalAcceptance,
 		}
 		c.JSON(http.StatusOK, resp)
-		utils.LogReqRespBody(req, resp, "CreateMember")
+		utils.LogReqRespBody(req, resp, "ReturnProblemPage")
 	}()
 
 	var Page_size = req.Page_Size
 	println(Page_size)
-	if result := ctl.DB.Find(&p); result.Error != nil {
+	/*if result := ctl.DB.Find(&p); result.Error != nil {
 		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
 			log.Println("Successfully get the Problem set!")
 			//resp := result
@@ -67,6 +68,24 @@ func (ctl ProblemController) ProblemList(c *gin.Context) {
 
 		}
 
+	}*/
+	/*problem = model.Problem{ProblemID: problemID, Status: status, Difficulty: difficulty, Acceptance: acceptance, GlobalAcceptance: globalAcceptance}*/
+	if result := ctl.DB.Find(&problem); result.Error != nil {
+		code = vo.UnknownError
+		log.Println("Error occured during get all problem information! ")
+		return
+	} else {
+
+		log.Println("The lenght of all problem :" + strconv.FormatInt(result.RowsAffected, 10))
+		/*result should be a slice*/
+
+		problemID = problem.ProblemID
+		status = problem.Status
+		difficulty = problem.Difficulty
+		acceptance = problem.Acceptance
+		globalAcceptance = problem.GlobalAcceptance
+
+		return
 	}
 	code = vo.UserHasExisted
 	log.Println("CreateMember:UserExisted")
@@ -77,8 +96,8 @@ func (ctl ProblemController) ProblemDetail(c *gin.Context) {
 	//TODO implement me
 	panic("implement me")
 	var req vo.ProblemDetailRequest
-	var problem model.Problem
-	//var descriptionModel model.Description
+	var problem, p model.Problem
+	var descriptionModel, d model.Description
 	code := vo.OK
 	var problemID = 0
 	var description = ""
@@ -97,11 +116,38 @@ func (ctl ProblemController) ProblemDetail(c *gin.Context) {
 			Global_Acceptance: globalAcceptance,
 		}
 		c.JSON(http.StatusOK, resp)
-		utils.LogReqRespBody(req, resp, "CreateMember")
+		utils.LogReqRespBody(req, resp, "ReturnProblemDescription")
 	}()
 
 	//descriptionModel = model.Description{ProblemID: req.ProblemID}
-	ctl.DB.Where("problem_id =?", req.ProblemID)
-	ctl.DB.Find(&problem)
-
+	problem = model.Problem{ProblemID: problemID, Status: status, Difficulty: difficulty, Acceptance: acceptance, GlobalAcceptance: globalAcceptance}
+	if err := ctl.DB.Where("problem_id =?", req.ProblemID).Take(&p).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			ctl.DB.First(&problem, req.ProblemID)
+			log.Println("Successfully find the problem detail information" + strconv.Itoa(problem.ProblemID))
+			problemID = problem.ProblemID
+			status = problem.Status
+			difficulty = problem.Difficulty
+			acceptance = problem.Acceptance
+			globalAcceptance = problem.GlobalAcceptance
+			return
+		} else {
+			code = vo.UnknownError
+			log.Println("Problem Information :Unknown-error while finding Problem information")
+			return
+		}
+	}
+	//ctl.DB.Find(&problem)
+	if err := ctl.DB.Where("problem_id =?", req.ProblemID).Take(&d).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			ctl.DB.First(&descriptionModel)
+			log.Println("Successfully find the problem detail information" + string(descriptionModel.Description))
+			description = string(descriptionModel.Description)
+			return
+		} else {
+			code = vo.UnknownError
+			log.Println("Problem Information :Unknown-error while finding Problem information")
+			return
+		}
+	}
 }
