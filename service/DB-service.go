71292a85327a0b472aeb6d3912f59634b1bda303
LeeTszFung <77471196+LeeTszFung@users.noreply.github.com>
2022-04-28 00:10:10 +0800

    DB-service trial

--- service/DB-service.go
+++ service/DB-service.go
@@ -2,40 +2,130 @@ package service
 
 import (
 	"context"
+	"errors"
+	"log"
+	"strconv"
+	"ustoj-master/common"
 	"ustoj-master/model"
+	"ustoj-master/vo"
 
 	"gorm.io/gorm"
 )
 
 type DBService interface {
-	Login(Username string, Password string) string
-	GetProblemList(map[string]interface{}) ([]*model.Problem, error)
-	ProblemDetail(map[string]interface{}) (*model.Problem, error)
-	/*...........Other still missing..........*/
+	CreateUser(user model.User) vo.ErrNo
+	Login(user model.User) string
+	//GetProblemList(map[string]interface{}) ([]*model.Problem, error)
+	GetProblemList(problem []model.Problem) []model.Problem
+	ProblemDetail(problemID string) model.Problem
+	ProblemDescription(problemID string) model.Description
+	Submission(submission model.Submission)
+	ResultList(submitted model.Submission) model.Submission
 }
 type DBConnect struct {
 	DB  *gorm.DB
 	Ctx context.Context
 }
 
-/*func NewDBConnect(db *gorm.DB) DBService { // Similar to the interface of service
+func NewDBConnect(DB *gorm.DB) DBService { // Similar to the interface of service
 	return &DBConnect{DB: common.GetDB(), Ctx: common.GetCtx()}
-}*/
+}
 
 /*func NewDBService() DBService {
 	 retrun
 }*/
 
-func Login(Username string, Password string) string {
-	test := ""
-	/*if err := DBConnect.DB.Where("user_name = ?", Username).Where("password =?", Password).Take(&u).Error; err != nil {
+func (db *DBConnect) CreateUser(user model.User) vo.ErrNo {
+	var u model.User
+	if err := db.DB.Where("user_name = ?", user.Username).Take(&u).Error; err != nil {
 		if errors.Is(err, gorm.ErrRecordNotFound) {
-			loginobject.Data.UserID = u.Username
+			db.DB.Create(&user)
+			log.Println("CreateMember:Successfully create, username:" + user.Username)
+			return vo.OK
 		} else {
-			code = vo.UnknownError
-			log.Println("Not User record")
-			return
+			log.Println("CreateMember:Unknown-error while creating")
+			return vo.UnknownError
 		}
-	}*/
-	return test
+	}
+	return vo.UnknownError
+}
+
+func (db *DBConnect) Login(user model.User) string {
+	var u model.User
+	if err := db.DB.Where("user_name = ?", user.Username).Where("password =?", user.Password).Take(&u).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return u.Username
+		} else {
+			log.Println("Not User record")
+			return "UnknownError"
+		}
+	}
+	return "UnknownError"
+}
+func (db *DBConnect) GetProblemList(problem []model.Problem) []model.Problem {
+	var problemlist []model.Problem
+	if result := db.DB.Find(&problem); result.Error != nil {
+		log.Println("Error occured during get all problem information! ")
+		return problemlist
+	} else {
+
+		log.Println("The lenght of all problem :" + strconv.FormatInt(result.RowsAffected, 10))
+
+		problemlist = problem
+
+		return problemlist
+	}
+}
+func (db *DBConnect) ProblemDetail(problemID string) model.Problem {
+	var problem, p model.Problem
+	if err := db.DB.Where("problem_id =?", problemID).Take(&p).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			db.DB.First(&problem, problemID)
+			log.Println("Successfully find the problem detail information" + strconv.Itoa(problem.ProblemID))
+
+			return problem
+		} else {
+
+			return problem
+		}
+	}
+	return problem
+}
+func (db *DBConnect) ProblemDescription(problemID string) model.Description {
+	var descriptionModel, d model.Description
+	if err := db.DB.Where("problem_id =?", problemID).Take(&d).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			db.DB.First(&descriptionModel)
+			log.Println("Successfully find the problem detail information" + string(descriptionModel.Description))
+
+			return descriptionModel
+		} else {
+
+			return descriptionModel
+		}
+	}
+	return descriptionModel
+}
+func (db *DBConnect) Submission(submission model.Submission) {
+	if err := db.DB.Create(&submission).Error; err != nil {
+		log.Println("Submission Error")
+
+	}
+
+}
+func (db *DBConnect) ResultList(submitted model.Submission) model.Submission {
+	var submission, s model.Submission
+	if err := db.DB.Where("problem_id =?", submitted.ProblemID).Where("username = ?", submitted.Username).Take(&s).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			db.DB.First(&submission)
+			log.Println("Successfully find the problem result" + strconv.Itoa(submission.ProblemID))
+
+			return submission
+		} else {
+
+			return submission
+		}
+	}
+	return submission
+
 }
