f4f3a3d85481594a0a8065f7892ebc43f992f2ec
kmftcy <kmftangchaoyang@gmail.com>
2022-05-07 09:07:20 +0000

    apiserver auto create schema

--- api-server/main.go
+++ api-server/main.go
@@ -1,20 +1,32 @@
 package main
 
 import (
-	"os"
+	"flag"
 	"ustoj-master/api-server/model"
 	"ustoj-master/common"
+	commonModel "ustoj-master/model"
 	appConfig "ustoj-master/scheduler/model"
 
 	"github.com/spf13/viper"
 )
 
 func main() {
-	common.ReadConfig(os.Args[1])
+	var configPath = flag.String("c", "/etc/ustoj/master-apiserver/config.yaml", "the file path to config file")
+	flag.Parse()
+
+	common.ReadConfig(*configPath)
 	model.InitConfig()
 	common.InitLogger(appConfig.Cfg.Logger.WriteFile)
 	common.InitDb(appConfig.Cfg.Logger.Level)
+	InitTable()
 	r := RegisterRouter()
 	port := viper.GetString("server.port")
 	r.Run("0.0.0.0:" + port)
+}
+
+func InitTable() {
+	common.CreateTableIfNotExists(commonModel.User{})
+	common.CreateTableIfNotExists(commonModel.Problem{})
+	common.CreateTableIfNotExists(commonModel.Description{})
+	common.CreateTableIfNotExists(commonModel.Submission{})
 }
